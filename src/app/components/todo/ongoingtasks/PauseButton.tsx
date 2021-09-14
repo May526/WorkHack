0058b0768197d5eab165ef2560c112242a4c0da1
kang <61167784+kang-jp@users.noreply.github.com>
2021-09-14 12:21:15 +0900

    add:一時停止機能について, 時間データもデータベースに記録

--- src/app/components/todo/ongoingtasks/PauseButton.tsx
+++ src/app/components/todo/ongoingtasks/PauseButton.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Button } from 'reactstrap'
-import { updateTask } from '../../../../database/database_write';
+import { addPausedTime, addUnpausedTime, updateTask } from '../../../../database/database_write';
 import { task } from '../../../../lib/types'
 
 export default function PauseButton(props:{task:task,project_id:string,task_id:string}) {
 
     const {task,project_id,task_id} = props;
     const onClick = () => {
+        if(task.is_paused){
+            addUnpausedTime(project_id,task_id,new Date());
+        }else{
+            addPausedTime(project_id,task_id,new Date());
+        };
+
         updateTask(project_id,task_id,"is_paused",!(task.is_paused));
     }
 
