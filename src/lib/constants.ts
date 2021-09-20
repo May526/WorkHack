660cf026863c5e2b9a50589dc1f2299fd448a46c
kang <61167784+kang-jp@users.noreply.github.com>
2021-09-20 11:34:11 +0900

    fix:感情の名前についてリファクタリング

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const PLEASANT_ENEGY_LABELS = ["わくわく", "楽しい", "嬉しい"]
+export const UNPLEASANT_ENEGY_LABELS = ["ストレス", "緊張", "いらいら"]
+export const UNPLEASANT_UNENEGY_LABELS = ["疲れた", "退屈", "うんざり"]
+export const PLEASANT_UNENEGY_LABELS = ["リラックス", "落ち着いている", "癒し"]
