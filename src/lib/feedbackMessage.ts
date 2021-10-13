6603cede825fd9c6e2fb38f04f861d350468107a
kang <61167784+kang-jp@users.noreply.github.com>
2021-10-13 18:54:51 +0900

    update:libからmessageを取得する形にリファクタリング

---
+++ src/lib/feedbackMessage.ts
@@ -0,0 +1,23 @@
+export const getGreeting = () => {
+    return "今日もお疲れ様です。"
+}
+
+export const getYesterdayReviewMessage = () => {
+    return "昨日は穏やかだったようですね。"
+}
+
+export const getDailyReviewMessage = () => {
+    return "最近、朝にイライラ/緊張/ストレスを感じることが多いようです。"
+}
+
+export const getActionRecommendTopMessage = () => {
+    return "朝にイライラ/緊張/ストレスを感じるときは、以下の行動をしてみてはいかがでしょう？"
+}
+
+export const getRecommendedActionsMessages = () => {
+    return ["深呼吸してみましょう","深呼吸してみましょう"]
+}
+
+export const getEndingMessage = () => {
+    return "少しの行動で気持ちは前向きになっていきますよ。"
+}
